26306843a5ddca0b35ea4c7ec657eb7e7881117d
Joshua Gendal <joshuagendal@Joshuas-MacBook-Pro.local>
2021-01-08 13:58:14 -0500

    fixed view show url issue

--- codingdojochallenge/urls.py
+++ codingdojochallenge/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('new/', views.addShow, name='addShow'),
     # path('add-new-show/', views.addNewShow, name='addNewShow'),
-    path('<int:id>', views.viewShow, name='viewShow')
+    path('<int:show_id>', views.viewShow, name='viewShow')
 ]
