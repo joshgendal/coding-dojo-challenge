f6e149ff46bbb96d6da5ed6d19783ae58116fb20
Joshua Gendal <joshuagendal@Joshuas-MacBook-Pro.local>
2021-01-08 06:46:02 -0500

    add form file with showform and modify template to add django created form fields

--- codingdojochallenge/views.py
+++ codingdojochallenge/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from codingdojochallenge.forms import ShowForm
 
 def addShow(request):
-    return render(request, 'codingdojochallenge/add-show.html')
+    context = {}
+    context['form'] = ShowForm
+    return render(request, 'codingdojochallenge/add-show.html', context=context)
 
 def addNewShow(request):
     if request.method == 'POST':
-        title = request.POST.get('title')
-        network = request.POST.get('network')
-        date = request.POST.get('date')
-        description = request.POST.get('description')
-        print(title, network, date, description)
-    return HttpResponse(title)
+        # title = request.POST.get('title')
+        # network = request.POST.get('network')
+        # date = request.POST.get('date')
+        # description = request.POST.get('description')
+        form = ShowForm(request.POST)
+        if form.is_valid():
+            show_obj = form.save()
+            # show_obj.save()
+        # print(title, network, date, description)
+    return HttpResponse('did it work?s')
